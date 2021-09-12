5710fea82264c109c966787cf1a81107f138b062
Sina MohammadZadeh <sina75mn@outlook.com>
2021-09-12 22:23:12 +0430

    initial commit

---
+++ utils/string.js
@@ -0,0 +1,57 @@
+export function toFarsiNumber(n) {
+    if (n) {
+        const farsiDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+        return n.toString().replace(/\d/g, (x) => farsiDigits[x]);
+    } else return "";
+}
+
+const persianNumber = [
+    /۰/g,
+    /۱/g,
+    /۲/g,
+    /۳/g,
+    /۴/g,
+    /۵/g,
+    /۶/g,
+    /۷/g,
+    /۸/g,
+    /۹/g,
+];
+
+const arabicNumber = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g];
+
+export function fixNumbers(str) {
+    if (typeof str === "string") {
+        for (let i = 0; i < 10; i++) {
+            str = str.replace(persianNumber[i], i).replace(arabicNumber[i], i);
+        }
+    }
+    return str;
+}
+
+export function numberWithCommas(x) {
+    x = x.toString();
+    const pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x)) x = x.replace(pattern, "$1,$2");
+    return x;
+}
+
+export function persianNumbers(num) {
+    const num_dic = {
+        "۰": "0",
+        "۱": "1",
+        "۲": "2",
+        "۳": "3",
+        "۴": "4",
+        "۵": "5",
+        "۶": "6",
+        "۷": "7",
+        "۸": "8",
+        "۹": "9",
+    };
+    return parseInt(
+        num.replace(/[۰-۹]/g, function (w) {
+            return num_dic[w];
+        })
+    );
+}
