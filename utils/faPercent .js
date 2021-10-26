b3f2e58d7a98c3ba859c62dcf761994c9f82cdd7
negar faghih <negar.faghih68@gmail.com>
2021-10-26 12:22:28 +0330

    add all of function in one page (utils)

--- utils/faPercent .js
+++
@@ -1,7 +0,0 @@
-export function faPercent (x) {
-  const faPercent = new Intl.NumberFormat("fa", {
-    style: "percent",
-    minimumFractionDigits: 3
-  }).format;
-  return(faPercent(x));
-}
--- utils/persianDecimal .js
+++
@@ -1,7 +0,0 @@
-export function persianDecimal  (x) {
-  const persianDecimal = new Intl.NumberFormat("fa", {
-    minimumIntegerDigits: 2,
-    maximumFractionDigits: 2
-  });
-  return(persianDecimal.format(x));
-}
