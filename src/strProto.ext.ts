171b7f466a728491a653ffe33cf870c656778dd0
amirhooseindaliri <amirhataeidaliri1378@gmail.com>
2021-12-08 12:37:15 +0330

    seccond commit

--- src/strProto.ext.ts
+++
@@ -1,6 +0,0 @@
-interface String {
-    myCoolMethod(input: number): string;
-}
- String.prototype.myCoolMethod = function (inputValue: number): string {
-    return this + inputValue.toString();
-};
